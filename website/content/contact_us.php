f72077d8e2d000da56e518a55388ae8ae72ab1ac
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2016-11-28 20:17:54 +0100

    fixed navbar@

--- website/content/contact_us.php
+++ website/content/contact_us.php
@@ -14,39 +14,7 @@
     <body>
     
         <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php">Home</a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="https://campusnet.jacobs-university.de/">Campusnet</a>
-                        </li>
-                        <li>
-                            <a href="https://cantaloupe.eecs.jacobs-university.de/login.php?f=djExOA">jgrader</a>
-                        </li>
-                        <li>
-                            <a href="#">Video Materials</a>
-                        </li>
-                        <li>
-                            <a href="contact_us.html">Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-            <!-- /.container -->
-        </nav>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/inc/navbar.php'; ?>
         <!-- Content Section -->
         <section>
             <div class="container">
